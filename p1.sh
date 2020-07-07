790bd3902b12dbf683935b783a71c52b88130834
André Tiago <andre@openSUSE-DESKTOP.lan>
2020-07-07 18:59:15 +0100

    atualizado scripts

--- p1.sh
+++ p1.sh
@@ -8,14 +8,22 @@ output_dir=p1_output
 mkdir -p $output_dir
 
 sort_dir=prog_sort
-make -C $sort_dir
+
+module load gcc/5.3.0
 
 function a_c {
+    make -C $sort_dir clean
+    make -C $sort_dir
+
     perf stat -e instructions,cycles,cache-misses,cache-references,branches,branch-misses $sort_dir/sort $1 1 100000000 2> $output_dir/1a_sort$1
 
     perf record -F 99 $sort_dir/sort $1 1 100000000
     perf report -n --stdio > $output_dir/1b_sort$1
 
+    perf record -F 99 -ag $sort_dir/sort $1 1 100000000
+    perf script | ./stackcollapse-perf.pl | ./flamegraph.pl> $output_dir/sort$1_O3.svg
+
+    g++ $sort_dir/*.c $sort_dir/sort
     perf record -F 99 -ag $sort_dir/sort $1 1 100000000
     perf script | ./stackcollapse-perf.pl | ./flamegraph.pl> $output_dir/sort$1.svg
 }
@@ -24,3 +32,5 @@ a_c 1
 a_c 2
 a_c 3
 a_c 4
+
+module unload gcc/5.3.0
--- p2.sh
+++ p2.sh
@@ -4,33 +4,54 @@ set -x
 
 #cd $HOME/ESC/TP4
 
-gcc -ggdb naive.c -o naive
+output_dir=p2_output
+mkdir -p $output_dir
 
 function parte_1 {
-    perf stat -e cpu-clock,faults ./naive
+    gcc -ggdb naive.c -o naive
 
     perf record -e cpu-clock,faults ./naive
-    perf report --stdio --dsos=naive,libc-2.12.so
+    perf report --stdio --dsos=naive,libc-2.12.so > $output_dir/p1_report
 
-    perf annotate --stdio --dsos=naive --symbol=multiply_matrices --source
-
-    # Devemos ter pelo menos 100 a 500 samples
-    # Podemos usar --freq= para aumentar a frequência de sampling -> vai ter mais overhead
-
-    # perf evlist -F
+    perf annotate --stdio --dsos=naive --symbol=multiply_matrices --source > $output_dir/p1_annotate
 }
 
 function parte_2 {
-    gcc -ggdb naive.c -o naive -O3
-    gcc -ggdb interchange.c -o interchange -O3
+    gcc naive.c -o naive -O3
+    gcc interchange.c -o interchange -O3
     
-    perf stat -e cpu-cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,branch-loads,branch-load-misses ./naive
+    perf stat -e cpu-cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,branch-loads,branch-load-misses ./naive 2> $output_dir/p2_naive
     
-    perf stat -e cpu-cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,branch-loads,branch-load-misses ./interchange
-    
-    #bus-cycles
-    #L1-icode-load-misses
+    perf stat -e cpu-cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,branch-loads,branch-load-misses ./interchange 2> $output_dir/p2_interchange
 }
 
-#parte_1
+function parte_3 {
+    gcc -ggdb naive.c -o naive
+    
+    perf record -e cpu-cycles -c 100000 ./naive
+    perf report -n --no-source --stdio --percent-limit 0.01 > $output_dir/p3_report
+    perf annotate --stdio --dsos=naive --symbol=multiply_matrices --no-source > $output_dir/p3_annotate
+
+    perf record -c 100000 -e cpu-cycles -ag ./naive
+    perf script | ./stackcollapse-perf.pl | ./flamegraph.pl > naive_cpu-cycles_C100000.svg
+
+    perf record -F 99 -e cpu-cycles -ag ./naive
+    perf script | ./stackcollapse-perf.pl | ./flamegraph.pl > naive_cpu-cycles_F99.svg
+
+    gcc -O3 naive.c -o naive
+    perf record -e cpu-cycles,instructions -c 100000 ./naive
+    perf report -n --stdio > $output_dir/p3_cpu-cycles_instructions
+    
+    perf record -e cache-references,cache-misses -c 100000 ./naive 
+    perf report -n --stdio > $output_dir/p3_cache-references_cache-misses
+    
+    perf record -e LLC-loads,LLC-load-misses -c 100000 ./naive 
+    perf report -n --stdio > $output_dir/p3_LLC-loads_LLC-load-misses
+    
+    perf record -e branch-loads,branch-load-misses -c 100000 ./naive 
+    perf report -n --stdio > $output_dir/p3_branch-loads_branch-load-misses
+}
+
+parte_1
 parte_2
+parte_3
